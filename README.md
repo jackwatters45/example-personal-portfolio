76c3d60945da988865855acdfd780219b4fa1633
Jack Watters <johnwatt@usc.edu>
2023-04-16 20:43:48 -0700

    Responsiveness improvements. minor

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 ## todo
 
-- improve responsiveness
 - check out third party libraries
 - add animations
+
+  - scroll etc for tech stack
+  - hover for projects
+  - some kind of animation for the intro
+  - header animations
+
 - preview images -> video
+  - if not just make sure they look good when big
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -12,21 +12,19 @@ const Contact = () => {
           Please get in touch if you think our work could be mutually
           beneficial!
         </p>
-<div className="secondRow">
-
-        <div className="address">
-          <Icon path={mdiHome} size={1} />
-          <p>17 Plymouth Ave Mill Valley, CA 94941</p>
-        </div>
-        <div className="phone">
-          <Icon path={mdiPhone} size={1} />
-          <p>(954) 494-9167</p>
-        </div>
-        <div className="email">
-          <Icon path={mdiEmail} size={1} />
-          <p>jack.watters@me.com</p>
-        </div>
-        <div className="icons">
+        <div className="secondRow">
+          <div className="address">
+            <Icon path={mdiHome} size={1} />
+            <p>17 Plymouth Ave Mill Valley, CA 94941</p>
+          </div>
+          <div className="phone">
+            <Icon path={mdiPhone} size={1} />
+            <p>(954) 494-9167</p>
+          </div>
+          <div className="email">
+            <Icon path={mdiEmail} size={1} />
+            <p>jack.watters@me.com</p>
+          </div>
           <a href="https://github.com/jackwatters45">
             <Icon path={mdiGithub} size={1} />
           </a>
@@ -34,7 +32,6 @@ const Contact = () => {
             <Icon path={mdiLinkedin} size={1} />
           </a>
         </div>
-</div>
       </div>
     </section>
   );
